02a2184fa95227d0226d06f75d39120b5cb235d0
KIA <aeddi84@gmail.com>
2026-05-24 08:31:09 +0000

    exo

---
+++ week1/day3/daily_challenge/day.py
@@ -0,0 +1,10 @@
+##3 mon day 1 
+# je veux les afficher tous sur la meme ligne hello world *4 en utilisant un retoure a la ligne \n
+# print("hello world\n" * 4)
+# Exercice 2 : Un peu de maths
+# Instructions
+# Écrivez un code qui calcule le résultat de :
+
+# (99^3)*8(ce qui signifie 99 à la puissance 3, fois 8).
+print((99**3)*8)
+# Exercice 3 : La ferme de McDonald
